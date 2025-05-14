b608513df41405ae0a48db2cf5ea812180dff4f6
Murat Alibek <71397200+muratalibek@users.noreply.github.com>
2025-05-15 01:20:17 +0500

    Added annotation @RestController to ApiApplication class Added method to return the list

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -2,12 +2,20 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+		SpringApplication.run(ApiApplication.class, args);}
 
+		@GetMapping(path = "/hello")
+        public List<String> hello() {
+			return List.of("Hello","world","with","spring","boot");
+	}
 }
